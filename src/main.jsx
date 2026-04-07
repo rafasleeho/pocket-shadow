d379ae06de79ca233c85f607f1aa6ae0c1af196e
rafasleeho <rafaelncastro@yahoo.com>
2026-04-07 17:26:34 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,3 @@
-import { supabase } from './lib/supabase'
-supabase.from('ps_prompts').select('*').limit(1).then(({ data, error }) => console.log('DB check:', data, error))
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './PocketShadow'
